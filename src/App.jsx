0c0c709995c618fe36323aadf361fcb5190fb6ff
olabiaga <jrolabiaga@gbox.ncf.edu.ph>
2024-12-17 11:56:20 +0800

    oki

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 // Importing components
 import Dashboard from './Dashboard';
 import Login from './Login';
